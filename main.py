6f4da554859b1f988df0f94e29b17fcd8f1d3c67
Eric Landstein <eric@landsteinmail.com>
2023-06-22 10:46:37 -0400

    Day 24: Add pdf pages

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=20)
     pdf.set_text_color(200,100,100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="C", ln=1, border=1)
-    pdf.line()
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+
 
 pdf.output("output.pdf")
