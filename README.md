69a52424f714175d95936179ea9ea6d629063813
Eric Landstein <eric@landsteinmail.com>
2023-06-22 12:56:31 -0400

    Day 25: Created script to load excel file data into dataframe

---
+++ README.md
@@ -0,0 +1,4 @@
+# What is this project
+
+Created PDF invoices from excel
+
--- main.py
+++ main.py
@@ -1,30 +1,9 @@
-from fpdf import FPDF
 import pandas as pd
+import glob
 
-df = pd.read_csv("topics.csv")
+filepaths = glob.glob("invoices/*.xlsx")
 
-pdf = FPDF(orientation="P", unit="mm", format="A4")
-pdf.set_auto_page_break(auto=False, margin=0)
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    print(df.head(1))
 
-for index, row in df.iterrows():
-    pdf.add_page()
-    pdf.set_font(family="Times", style="B", size=20)
-    pdf.set_text_color(200,100,100)
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="C", ln=1, border=1)
-
-    # Set Footer
-    pdf.ln(260)
-    pdf.set_font(family="Times", style="B", size=8)
-    pdf.set_text_color(200, 50, 150)
-    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
-
-    for i in range(row["Pages"] - 1):
-        pdf.add_page()
-        # Set Footer
-        pdf.ln(275)
-        pdf.set_font(family="Times", style="B", size=8)
-        pdf.set_text_color(200, 50, 150)
-        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
-
-
-pdf.output("output.pdf")
