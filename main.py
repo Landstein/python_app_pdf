7dab34d37bb9e7435a6ac6c74d272191b1eeb8cf
Eric Landstein <eric@landsteinmail.com>
2023-06-22 10:41:37 -0400

    Initial Commit created PDF

---
+++ main.py
@@ -0,0 +1,15 @@
+from fpdf import FPDF
+import pandas as pd
+
+df = pd.read_csv("topics.csv")
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=20)
+    pdf.set_text_color(200,100,100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="C", ln=1, border=1)
+    pdf.line()
+
+pdf.output("output.pdf")
